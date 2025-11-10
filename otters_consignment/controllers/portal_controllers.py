8ea9b1a637de6b6fb6d6f090528b597b9fc29326
Tom Hoornaert <tom.hoornaert@cegeka.com>
2025-11-10 12:53:32 +0100

    Otters En Flamingos: betere overzicht in klantenportaal wat verkocht is, wat uitbetaald is en wat nog te koop staat

--- otters_consignment/controllers/portal_controllers.py
+++ otters_consignment/controllers/portal_controllers.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 from odoo.http import request
-from odoo.addons.portal.controllers.portal import CustomerPortal, pager as portal_pager
-import logging
+from odoo.addons.portal.controllers.portal import CustomerPortal
+from odoo.osv import expression
 
+# --- FIX 1: Imports toegevoegd voor AccessError en MissingError ---
+from odoo.exceptions import AccessError, MissingError
+
+import logging
 _logger = logging.getLogger(__name__)
 
 class ConsignmentPortal(CustomerPortal):
 
+    # Dit voegt de teller toe aan *alle* portal paginas (voor o.a. broodkruimels)
     def _prepare_portal_layout_values(self):
-        """
-        Deze functie blijft nodig voor de *andere* portal paginas.
-        """
         values = super(ConsignmentPortal, self)._prepare_portal_layout_values()
-
-        # We voegen de teller hier ook toe voor consistentie
         partner = request.env.user.partner_id
         submission_count = request.env['otters.consignment.submission'].sudo().search_count([
             ('supplier_id.email', '=ilike', partner.email)
@@ -22,109 +22,103 @@ class ConsignmentPortal(CustomerPortal):
         values['consignment_count'] = submission_count
         return values
 
+    # --- FIX 3: Functienaam gewijzigd naar _list ---
+    # Dit is de LIJST-pagina (/my/consignments)
     @http.route(['/my/consignments', '/my/consignments/page/<int:page>'], type='http', auth="user", website=True)
-    def portal_my_consignments(self, page=1, **kw):
-        """ Toont de lijst van alle inzendingen. """
-
+    def portal_my_consignments_list(self, page=1, sortby=None, filterby=None, search=None, search_in='all', **kw):
         values = self._prepare_portal_layout_values()
         partner = request.env.user.partner_id
-        # Zoek met sudo om te garanderen dat de ACL's van de Portal-gebruiker de telling niet blokkeren
-        Submission = request.env['otters.consignment.submission'].sudo()
+        Submission = request.env['otters.consignment.submission']
 
         domain = [('supplier_id.email', '=ilike', partner.email)]
 
-        pager_values = portal_pager(
+        searchbar_sortings = {'date': {'label': 'Datum', 'order': 'submission_date desc'}}
+        if not sortby:
+            sortby = 'date'
+        order = searchbar_sortings[sortby]['order']
+
+        pager_values = request.website.pager(
             url="/my/consignments",
-            total=values['consignment_count'],
+            total=Submission.sudo().search_count(domain),
             page=page,
             step=self._items_per_page
         )
-
-        submissions = Submission.search(domain, limit=self._items_per_page, offset=pager_values['offset'])
+        submissions = Submission.sudo().search(domain, order=order, limit=self._items_per_page, offset=pager_values['offset'])
 
         values.update({
             'submissions': submissions,
-            'page_name': 'consignment',
-            'default_url': '/my/consignments',
+            'page_name': 'consignment_list',
             'pager': pager_values,
+            'default_url': '/my/consignments',
+            'searchbar_sortings': searchbar_sortings,
+            'sortby': sortby,
         })
         return request.render("otters_consignment.portal_my_consignments", values)
 
-
-    def _get_submission_check_access(self, submission_id):
-        """ Controleert of de ingelogde gebruiker toegang heeft tot dit record op basis van e-mail. """
-        # Gebruik .browse(id).sudo() om het record op te halen buiten ACL-beperkingen
-        submission = request.env['otters.consignment.submission'].browse(submission_id).sudo()
-        partner = request.env.user.partner_id
-
-        # Verbeterde toegangscontrole via e-mailadres
-        if not submission or submission.supplier_id.email.lower() != partner.email.lower():
-            raise request.exceptions.AccessError('U hebt geen toegang tot deze inzending.')
-
-        return submission
-
+    # --- FIX 3: Functienaam gewijzigd naar _detail ---
+    # Dit is de DETAIL-pagina (/my/consignments/<id>)
     @http.route(['/my/consignments/<int:submission_id>'], type='http', auth="user", website=True)
-    def portal_submission_page(self, submission_id, **kw):
-        """ Toont de details van één specifieke inzending. """
+    def portal_my_consignments_detail(self, submission_id, access_token=None, **kw):
         try:
-            submission = self._get_submission_check_access(submission_id)
-        except request.exceptions.AccessError:
-            return request.render("website.403") # Geen toegang
+            # --- FIX 2: 'access_token' verwijderd uit de aanroep ---
+            submission_sudo = self._get_submission_check_access(submission_id)
+        except (AccessError, MissingError): # Werkt nu dankzij Fix 1
+            return request.redirect('/my')
 
-        # --- LOGICA VOOR VERKOCHTE VS. IN VOORRAAD ---
-        products_sold = submission._get_sold_products()
-        products_in_stock = submission.product_ids - products_sold
+        report_model = request.env['otters.consignment.report']
 
-        # --- NIEUWE LOGICA VOOR VERKOOPGEGEVENS (PRIJS & COMMISSIE) ---
-        # 1. Haal de rapportlijnen op voor de verkochte producten in deze inzending
-        report_lines = request.env['otters.consignment.report'].sudo().search([
-            ('supplier_id', '=', submission.supplier_id.id),
-            ('product_id', 'in', products_sold.ids),
+        all_sold_products = submission_sudo._get_sold_products()
+        stock_products = request.env['product.template'].sudo().search([
+            ('submission_id', '=', submission_id),
+            ('id', 'not in', all_sold_products.ids),
+            ('active', '=', True)
         ])
 
-        # 2. Aggregeer de verkoopgegevens per product (nodig als een product meerdere keren verkocht is)
-        product_report_map = {}
-        for line in report_lines:
-            product_id = line.product_id.id
-            if product_id not in product_report_map:
-                product_report_map[product_id] = {
-                    'total_sold_price': 0.0,
-                    'total_commission': 0.0,
-                    'qty_sold': 0,
-                }
-            # De rapportlijn bevat de subtotalen van de SO line, deze tellen we op
-            product_report_map[product_id]['total_sold_price'] += line.price_subtotal
-            product_report_map[product_id]['total_commission'] += line.commission_amount
-            product_report_map[product_id]['qty_sold'] += line.qty_sold
+        all_report_lines = report_model.sudo().search([('submission_id', '=', submission_id)])
 
+        def aggregate_report_lines(lines):
+            aggregated_data = {}
+            for line in lines:
+                product = line.product_id
+                if product not in aggregated_data:
+                    aggregated_data[product] = {'name': product.name, 'price_sold': 0, 'payout': 0, 'qty': 0}
 
-        products_sold_data = []
-        total_payout = 0.0
+                aggregated_data[product]['price_sold'] += line.price_subtotal
+                aggregated_data[product]['payout'] += line.commission_amount
+                aggregated_data[product]['qty'] += line.qty_sold
+            return aggregated_data.values()
 
-        # 3. Koppel de data aan de verkochte producten
-        for product in products_sold:
-            report_data = product_report_map.get(product.id, {})
+        unpaid_lines = all_report_lines.filtered(lambda r: not r.x_is_paid_out)
+        paid_lines = all_report_lines.filtered(lambda r: r.x_is_paid_out)
 
-            sold_price_total = report_data.get('total_sold_price', 0.0)
-            commission_total = report_data.get('total_commission', 0.0)
-            qty_sold = report_data.get('qty_sold', 0)
+        aggregated_unpaid = aggregate_report_lines(unpaid_lines)
+        aggregated_paid = aggregate_report_lines(paid_lines)
 
-            products_sold_data.append({
-                'product': product,
-                'qty_sold': qty_sold,
-                'total_sold_price': sold_price_total,
-                'total_commission': commission_total,
-            })
-            total_payout += commission_total # Bereken de totale uitbetaling
+        total_payout_unpaid = sum(l.commission_amount for l in unpaid_lines)
+        # (Ik heb je typo hier ook gecorrigeerd: pMayout -> payout)
+        total_payout_paid = sum(l.commission_amount for l in paid_lines)
 
-        values = self._prepare_portal_layout_values()
-        values.update({
-            'submission': submission,
-            'products_in_stock': products_in_stock,
-            'products_sold_data': products_sold_data, # NIEUW: Bevat prijs en commissie
-            'total_payout_for_submission': total_payout,
-            'page_name': 'submission',
-            'pager': False,
-            'currency': request.env.company.currency_id, # Valuta van het bedrijf
-        })
+        values = {
+            'submission': submission_sudo,
+            'stock_products': stock_products,
+            'aggregated_unpaid': aggregated_unpaid,
+            'aggregated_paid': aggregated_paid,
+            'total_payout_unpaid': total_payout_unpaid,
+            'total_payout_paid': total_payout_paid,
+            'page_name': 'consignment_submission',
+            'access_token': access_token,
+        }
+
         return request.render("otters_consignment.portal_consignment_submission", values)
+
+    # Dit is de helper-functie voor toegangscontrole
+    def _get_submission_check_access(self, submission_id):
+        submission = request.env['otters.consignment.submission'].browse([submission_id])
+        if not submission.exists():
+            raise MissingError("Deze inzending bestaat niet.")
+
+        partner = request.env.user.partner_id
+        if submission.sudo().supplier_id.email != partner.email:
+            raise AccessError("Je hebt geen toegang tot deze inzending.")
+
+        return submission.sudo()
