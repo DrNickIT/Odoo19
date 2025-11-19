c7d3c3db6dbef7065c59f7a055ae882751ce31ca
Tom Hoornaert <tom.hoornaert@cegeka.com>
2025-11-19 21:55:09 +0100

    Otters En Flamingos: nieuwe module website_outfit

---
+++ website_outfit/__manifest__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Website Outfits",
+    'summary': "Beheer en toon outfits op je website.",
+    'description': """
+        Staat toe om outfits te maken (bestaande uit meerdere producten) 
+        en deze te tonen op de website via een snippet en detailpagina's.
+    """,
+    'author': "Jouw Naam",
+    'website': "https_jouw_website.nl",
+    'category': 'Website/eCommerce',
+    'version': '19.0.1.0.0',
+
+    # Essentiële afhankelijkheden
+    'depends': [
+        'base',
+        'website',
+        'website_sale', # Nodig voor /shop/cart functionaliteit
+        'product',        # Nodig voor de M2M relatie
+    ],
+
+    # Laad al onze nieuwe bestanden
+    'data': [
+        'security/ir.model.access.csv',
+        'views/outfit_views.xml',
+        'views/outfit_templates.xml',
+        'views/outfit_assets.xml',
+    ],
+
+    # Voeg een icoon toe (optioneel, maar netjes)
+    # Maak hiervoor een bestand: /addons/website_outfit/static/description/icon.png
+    'images': ['static/description/icon.png'],
+
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
---
+++ website_outfit/models/outfit.py
@@ -0,0 +1,51 @@
+from odoo import models, fields, api
+# GEEN foute imports
+
+class Outfit(models.Model):
+    _name = 'website.outfit'
+    _description = 'Website Outfit'
+    _inherit = ['website.published.mixin', 'website.multi.mixin']
+
+    name = fields.Char('Name', required=True, translate=True)
+    description = fields.Text('Description', translate=True)
+
+    image = fields.Image('Photo', max_width=1920, max_height=1920)
+
+    # --- HIER HET NIEUWE VELD ---
+    # Dit is de enige wijziging die je in dit bestand hoeft te maken
+    sale_ok = fields.Boolean('Can be Sold', default=True)
+    # -----------------------------
+
+    product_ids = fields.Many2many(
+        'product.product',
+        string='Products',
+        domain="[('sale_ok', '=', True)]"
+    )
+
+    slug = fields.Char(
+        'Slug',
+        compute='_compute_slug',
+        store=True,
+        help="De URL-vriendelijke versie van de naam."
+    )
+
+    website_url = fields.Char(
+        compute='_compute_website_url',
+        help='De volledige URL naar de outfit.'
+    )
+
+    @api.depends('name')
+    def _compute_slug(self):
+        for outfit in self:
+            if outfit.id and outfit.name:
+                outfit.slug = f"{self.env['ir.http']._slug(outfit)}-{outfit.id}"
+            else:
+                outfit.slug = False
+
+    @api.depends('slug')
+    def _compute_website_url(self):
+        for outfit in self:
+            if outfit.slug:
+                outfit.website_url = f'/outfit/{outfit.slug}'
+            else:
+                outfit.website_url = False
