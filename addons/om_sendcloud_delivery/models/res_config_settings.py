a95cce5d0214c3cd6c333e6ecd836a5a5e656709
Tom Hoornaert <tom.hoornaert@cegeka.com>
2025-11-22 01:48:57 +0100

    Otters En Flamingos: Settings sendcloud module naar settings verplaatst

--- addons/om_sendcloud_delivery/models/res_config_settings.py
+++ addons/om_sendcloud_delivery/models/res_config_settings.py
@@ -3,15 +3,20 @@ from odoo import models, fields
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    # We linken deze velden direct aan de velden op het bedrijf (res.company)
-    # readonly=False is nodig zodat je ze ook kunt opslaan vanuit de settings
+    # De schakelaar (verbonden met het bedrijf)
+    is_sendcloud_enabled = fields.Boolean(
+        related='company_id.is_sendcloud_enabled',
+        readonly=False,
+        string="Sendcloud Connector (Community)"
+    )
+
     sendcloud_public_key = fields.Char(
         related='company_id.sendcloud_public_key',
         readonly=False,
-        string="Sendcloud Public Key (Community)"
+        string="Sendcloud Public Key"
     )
     sendcloud_secret_key = fields.Char(
         related='company_id.sendcloud_secret_key',
         readonly=False,
-        string="Sendcloud Secret Key (Community)"
+        string="Sendcloud Secret Key"
     )
