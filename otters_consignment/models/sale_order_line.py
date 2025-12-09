a5b81b2de1f2618b1632e5d132b81936aa31e808
Tom Hoornaert <tom.hoornaert@gmail.com>
2025-12-09 01:28:55 +0100

    Opkuis en verbetering

--- otters_consignment/models/sale_order_line.py
+++ otters_consignment/models/sale_order_line.py
@@ -15,23 +15,3 @@ class SaleOrderLine(models.Model):
 
     # NIEUW: Om duplicaten te voorkomen bij herstarten van migratie
     x_old_id = fields.Char(string="Oud Bestelregel ID", index=True, readonly=True)
-
-    def write(self, vals):
-        """
-        Als 'x_is_paid_out' op True wordt gezet (door Marleen via de actie),
-        dan vergrendelen we de bijbehorende inzending.
-        """
-        # 1. Voer de wijziging eerst uit
-        res = super(SaleOrderLine, self).write(vals)
-
-        # 2. Check of er uitbetaald wordt
-        if vals.get('x_is_paid_out'):
-            # Zoek de unieke inzendingen die aan deze lijnen hangen
-            # self kan hier bv. 50 geselecteerde regels zijn
-            submissions = self.mapped('product_template_id.submission_id')
-
-            if submissions:
-                # Zet ze op slot (alleen degene die nog niet gelockt waren)
-                submissions.filtered(lambda s: not s.x_is_locked).sudo().write({'x_is_locked': True})
-
-        return res
