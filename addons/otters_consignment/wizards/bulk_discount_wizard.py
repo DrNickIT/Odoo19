acb1e32ab2adb8caad91ef1d0673c41f360d268d
Tom Hoornaert <tom.hoornaert@gmail.com>
2025-12-12 02:06:15 +0100

    * fix bulk discount wizard

--- addons/otters_consignment/wizards/bulk_discount_wizard.py
+++ addons/otters_consignment/wizards/bulk_discount_wizard.py
@@ -8,13 +8,6 @@ class BulkDiscountWizard(models.TransientModel):
     discount_percentage = fields.Integer(string="Korting (%)", required=True, default=20)
     discount_reason = fields.Char(string="Reden", required=True, default="Solden / Opruiming")
 
-    class BulkDiscountWizard(models.TransientModel):
-        _name = 'otters.consignment.bulk.discount.wizard'
-    _description = 'Bulk Korting Toepassen'
-
-    discount_percentage = fields.Integer(string="Korting (%)", required=True, default=20)
-    discount_reason = fields.Char(string="Reden", required=True, default="Solden / Opruiming")
-
     def action_apply_bulk(self):
         """ Je bestaande functie (ongewijzigd) """
         active_ids = self.env.context.get('active_ids', [])
