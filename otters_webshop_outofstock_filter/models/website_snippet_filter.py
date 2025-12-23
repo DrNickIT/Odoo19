a1417105ba50cf7691c76fad6151355fd5dd5d2b
Tom Hoornaert <tom.hoornaert@gmail.com>
2025-12-23 02:44:30 +0100

    sorteer eerst op producten dan op services

--- otters_webshop_outofstock_filter/models/website_snippet_filter.py
+++ otters_webshop_outofstock_filter/models/website_snippet_filter.py
@@ -10,24 +10,43 @@ class WebsiteSnippetFilter(models.Model):
     @api.model
     def _get_products(self, mode, context):
         """
-        Override de _get_products om de SQL-zoekopdracht te beïnvloeden.
+        Override de _get_products om de 'Newest Products' snippet aan te passen.
         """
-        # Context kopiëren
+        # 1. Context kopiëren en filter instellen (Uitverkocht verbergen)
+        # Dit zorgt ervoor dat de database meteen de juiste items pakt (geen gatenkaas).
         context = context.copy() if context else {}
-
-        # Haal het bestaande domein op
         current_domain = context.get('search_domain') or []
 
-        # Onze stock-filter: qty > 0 EN virtual > 0
-        stock_domain = [('qty_available', '>', 0), ('virtual_available', '>', 0)]
+        # Logica: Laat zien als (Service) OF (Fysiek > 0 EN Virtueel > 0)
+        stock_domain = [
+            '|',
+            ('type', '=', 'service'),
+            '&', ('qty_available', '>', 0), ('virtual_available', '>', 0)
+        ]
 
-        # Gebruik Domain.AND (Odoo 19+)
+        # Voeg samen met bestaande filters via de veilige Odoo methode
         new_domain = Domain.AND([current_domain, stock_domain])
-
-        # Zet terug in context
         context['search_domain'] = new_domain
 
-        return super()._get_products(mode, context)
+        # 2. Haal de producten op via de standaard Odoo functie
+        # Odoo voert nu de zoekopdracht uit met onze nieuwe filter.
+        products = super()._get_products(mode, context)
+
+        # 3. SORTERING TOEPASSEN (Python-side)
+        # We hebben nu een lijst met producten (bijv. de 8 nieuwste die op voorraad zijn).
+        # Nu schudden we die lijst zodat de diensten (cadeaubonnen) onderaan komen.
+
+        if products and products._name in ['product.product', 'product.template']:
+            # Stap A: Sorteer alles op datum (nieuwste eerst)
+            # (Dit is meestal al zo, maar we forceren het voor de zekerheid)
+            products = products.sorted(key=lambda p: p.create_date, reverse=True)
+
+            # Stap B: Sorteer stabiel op type
+            # 'service' begint met een S, 'consu'/'product' met een C of P.
+            # Door te sorteren op type (A-Z) komen de Services (S) vanzelf achteraan.
+            products = products.sorted(key=lambda p: p.type or '')
+
+        return products
 
     def _filter_records_to_values(self, records, is_sample=False, **kwargs):
         """
